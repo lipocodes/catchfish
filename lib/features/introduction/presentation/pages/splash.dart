c0e011f253f4352c807aade72e15d09c6aa52751
Lior <lipocodes@yahoo.com>
2022-04-02 17:04:20 +0300

    feature_introduction

--- lib/features/introduction/presentation/pages/splash.dart
+++ lib/features/introduction/presentation/pages/splash.dart
@@ -1,6 +1,8 @@
 import 'dart:async';
 import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
+import 'package:easy_localization/easy_localization.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class Splash extends StatefulWidget {
   const Splash({Key? key}) : super(key: key);
@@ -9,7 +11,7 @@ class Splash extends StatefulWidget {
   State<Splash> createState() => _SplashState();
 }
 
-class _SplashState extends State<Splash> {
+class _SplashState extends State<Splash> with SingleTickerProviderStateMixin {
   double rightPositionBird1 = 300.0;
   double topPositionBird1 = 300.0;
   double rightPositionBird2 = 300.0;
@@ -20,12 +22,21 @@ class _SplashState extends State<Splash> {
   double rightPositionBird4 = 300.0;
   late AudioPlayer advancedPlayer;
   late AudioCache audioCache;
+  late AnimationController animationController;
+  double steeringAngle = 0;
+  int enumerator = 0;
+  int remainingMilliseconds = 5000;
+  bool colorLoadingText = false;
 
   playSound() async {
-    advancedPlayer = AudioPlayer();
-    audioCache = AudioCache(
-        fixedPlayer: advancedPlayer, prefix: 'assets/sounds/introduction/');
-    await audioCache.play("tweet.mp3");
+    final prefs = await SharedPreferences.getInstance();
+    bool permissionAudio = prefs.getBool("permissionAudio") ?? true;
+    if (permissionAudio) {
+      advancedPlayer = AudioPlayer();
+      audioCache = AudioCache(
+          fixedPlayer: advancedPlayer, prefix: 'assets/sounds/introduction/');
+      await audioCache.play("tweet.mp3");
+    }
   }
 
   @override
@@ -34,8 +45,32 @@ class _SplashState extends State<Splash> {
     super.initState();
     playSound();
 
+    animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 5),
+    );
+
+    animationController.repeat();
+
     Timer.periodic(const Duration(milliseconds: 100), (Timer t) {
+      if (remainingMilliseconds == 0) {
+        t.cancel();
+      } else {
+        remainingMilliseconds -= 100;
+      }
       setState(() {
+        enumerator++;
+        if (enumerator < 5) {
+          steeringAngle += 0.3;
+          colorLoadingText = true;
+        } else if (enumerator < 15) {
+        } else if (enumerator < 25) {
+          steeringAngle -= 0.3;
+          colorLoadingText = false;
+        } else {
+          enumerator = 0;
+        }
+
         if (rightPositionBird1 >= 10.0) {
           rightPositionBird1 -= 10.0;
           topPositionBird1 -= 10.0;
@@ -63,26 +98,62 @@ class _SplashState extends State<Splash> {
       child: Scaffold(
         backgroundColor: Colors.white,
         body: Stack(children: [
-          Column(
-            children: [
-              SizedBox(
-                height: MediaQuery.of(context).size.height * 0.1,
-              ),
-              SizedBox(
-                height: MediaQuery.of(context).size.height * 0.7,
-                child: Image.asset(
-                  'assets/images/introduction/boat_steering.png',
-                  fit: BoxFit.cover,
-                  height: double.infinity,
-                  width: double.infinity,
-                  alignment: Alignment.center,
+          Container(
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage(
+                  'assets/images/introduction/fishing_boat.jpg',
                 ),
+                fit: BoxFit.cover,
               ),
-              SizedBox(
-                height: MediaQuery.of(context).size.height * 0.1,
-                child: const Text("asdasdasdasdasdasda"),
-              ),
-            ],
+            ),
+            child: Column(
+              children: [
+                SizedBox(
+                  height: MediaQuery.of(context).size.height * 0.2,
+                ),
+                SizedBox(
+                  height: MediaQuery.of(context).size.height * 0.5,
+                  //width: MediaQuery.of(context).size.width * 0.9,
+                  child: Transform.rotate(
+                    angle: steeringAngle,
+                    child: Image.asset(
+                      'assets/images/introduction/boat_steering.png',
+                      fit: BoxFit.cover,
+                      height: double.infinity,
+                      width: double.infinity,
+                      alignment: Alignment.center,
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 50.0,
+                ),
+                colorLoadingText
+                    ? SizedBox(
+                        height: MediaQuery.of(context).size.height * 0.1,
+                        child: const Text(
+                          "loading",
+                          style: TextStyle(
+                            fontSize: 32,
+                            fontWeight: FontWeight.w700,
+                            color: Colors.brown,
+                          ),
+                        ).tr(),
+                      )
+                    : SizedBox(
+                        height: MediaQuery.of(context).size.height * 0.1,
+                        child: const Text(
+                          "loading",
+                          style: TextStyle(
+                            fontSize: 32,
+                            fontWeight: FontWeight.w700,
+                            color: Colors.yellow,
+                          ),
+                        ).tr(),
+                      ),
+              ],
+            ),
           ),
           if (rightPositionBird1 > 0.0) ...[
             Positioned(
@@ -126,10 +197,10 @@ class _SplashState extends State<Splash> {
               ),
             )
           ],
-          if (rightPositionBird3 > 0.0) ...[
+          if (rightPositionBird4 > 0.0) ...[
             Positioned(
-              top: topPositionBird3,
-              right: rightPositionBird3,
+              top: topPositionBird4,
+              right: rightPositionBird4,
               child: SizedBox(
                 height: 100,
                 child: Image.asset(
