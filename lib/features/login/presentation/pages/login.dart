62cc81f029b8b9f27d86a2324006934394de4cf7
Lior <lipocodes@yahoo.com>
2022-04-11 13:44:20 +0300

    feature login

--- lib/features/login/presentation/pages/login.dart
+++ lib/features/login/presentation/pages/login.dart
@@ -10,6 +10,61 @@ class Login extends StatefulWidget {
 class _LoginState extends State<Login> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Colors.white,
+        body: Padding(
+          padding: const EdgeInsets.all(32.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Spacer(),
+              const FlutterLogo(size: 120.0),
+              const Spacer(),
+              const Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    "Hey there,\n Welcome back!",
+                    style: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  )),
+              const SizedBox(height: 8.00),
+              const Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    "Login to your account to continue!",
+                    style: TextStyle(
+                      fontSize: 16.0,
+                    ),
+                  )),
+              const Spacer(),
+              ElevatedButton(
+                child: const Text("Sign Up with Google"),
+                onPressed: () {
+                  print("aaaaaaaaaaaaaaaaaaa");
+                },
+              ),
+              const SizedBox(
+                height: 40.0,
+              ),
+              RichText(
+                text:
+                    const TextSpan(text: 'Already have an account?', children: [
+                  TextSpan(
+                    text: "Log in",
+                    style: TextStyle(
+                      decoration: TextDecoration.underline,
+                    ),
+                  ),
+                ]),
+              ),
+              const Spacer(),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
