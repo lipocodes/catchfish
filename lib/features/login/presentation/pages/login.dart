503c3cc7565fe85770ca6c7f589fbb3c00d2ddb5
Lior <lipocodes@yahoo.com>
2022-04-11 17:53:33 +0300

    feature login

--- lib/features/login/presentation/pages/login.dart
+++ lib/features/login/presentation/pages/login.dart
@@ -1,10 +1,9 @@
-import 'package:catchfish/features/introduction/presentation/pages/splash.dart';
-import 'package:catchfish/features/lobby/presentation/pages/lobby.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:provider/provider.dart';
 import 'package:catchfish/features/login/presentation/blocs/provider/google_sign_in.dart';
+import 'package:easy_localization/easy_localization.dart';
 
 class Login extends StatefulWidget {
   const Login({Key? key}) : super(key: key);
@@ -37,67 +36,69 @@ class _LoginState extends State<Login> {
               backgroundColor: Colors.white,
               body: Padding(
                 padding: const EdgeInsets.all(32.0),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    const Spacer(),
-                    const FlutterLogo(size: 120.0),
-                    const Spacer(),
-                    const Align(
-                        alignment: Alignment.centerLeft,
-                        child: Text(
-                          "Hey there,\n Welcome back!",
-                          style: TextStyle(
-                            fontSize: 24,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        )),
-                    const SizedBox(height: 8.00),
-                    const Align(
-                        alignment: Alignment.centerLeft,
-                        child: Text(
-                          "Login to your account to continue!",
-                          style: TextStyle(
-                            fontSize: 16.0,
-                          ),
-                        )),
-                    const Spacer(),
-                    ElevatedButton.icon(
-                      label: const Text("Sign Up with Google"),
-                      onPressed: () {
-                        final provider = Provider.of<GoogleSignInProvider>(
-                            context,
-                            listen: false);
-                        provider.googleLogin();
-                      },
-                      style: ElevatedButton.styleFrom(
-                          primary: Colors.white,
-                          onPrimary: Colors.black,
-                          minimumSize: const Size(double.infinity, 50)),
-                      icon: const FaIcon(FontAwesomeIcons.google,
-                          color: Colors.blue),
-                    ),
-                    const SizedBox(
-                      height: 40.0,
-                    ),
-                    RichText(
-                      text: const TextSpan(
-                          text: 'Already have an account?',
-                          children: [
-                            TextSpan(
-                              text: "Log in",
-                              style: TextStyle(
-                                decoration: TextDecoration.underline,
-                              ),
-                            ),
-                          ]),
-                    ),
-                    const Spacer(),
-                  ],
-                ),
+                child: form(),
               ),
             ),
           );
         });
+  }
+
+  Widget form() {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        //const Spacer(),
+        Image.asset(
+          "assets/images/introduction/boat_steering.png",
+        ),
+        const Spacer(),
+        Align(
+            alignment: Alignment.centerLeft,
+            child: Text(
+              "need_login".tr(),
+              style: const TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+            )),
+        const SizedBox(height: 8.00),
+        Align(
+            alignment: Alignment.centerLeft,
+            child: Text(
+              "login_your_account".tr(),
+              style: const TextStyle(
+                fontSize: 16.0,
+              ),
+            )),
+        const Spacer(),
+        ElevatedButton.icon(
+          label: const Text("Sign Up with Google"),
+          onPressed: () {
+            final provider =
+                Provider.of<GoogleSignInProvider>(context, listen: false);
+            provider.googleLogin();
+          },
+          style: ElevatedButton.styleFrom(
+              primary: Colors.white,
+              onPrimary: Colors.black,
+              minimumSize: const Size(double.infinity, 50)),
+          icon: const FaIcon(FontAwesomeIcons.google, color: Colors.blue),
+        ),
+        const SizedBox(
+          height: 40.0,
+        ),
+        RichText(
+          text: const TextSpan(text: 'Already have an account?', children: [
+            TextSpan(
+              text: "Log in",
+              style: TextStyle(
+                decoration: TextDecoration.underline,
+              ),
+            ),
+          ]),
+        ),
+        const Spacer(),
+      ],
+    );
   }
 }
---
+++ lib/features/login/presentation/widgets/form.dart
@@ -0,0 +1,64 @@
+import 'package:catchfish/features/login/presentation/blocs/provider/google_sign_in.dart';
+import 'package:easy_localization/easy_localization.dart';
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:provider/provider.dart';
+
+Widget form(BuildContext context) {
+  return Column(
+    mainAxisAlignment: MainAxisAlignment.center,
+    children: [
+      //const Spacer(),
+      Image.asset(
+        "assets/images/introduction/boat_steering.png",
+      ),
+      const Spacer(),
+      Align(
+          alignment: Alignment.centerLeft,
+          child: Text(
+            "need_login".tr(),
+            style: const TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+            ),
+          )),
+      const SizedBox(height: 8.00),
+      Align(
+          alignment: Alignment.centerLeft,
+          child: Text(
+            "login_your_account".tr(),
+            style: const TextStyle(
+              fontSize: 16.0,
+            ),
+          )),
+      const Spacer(),
+      ElevatedButton.icon(
+        label: const Text("Sign Up with Google"),
+        onPressed: () {
+          final provider =
+              Provider.of<GoogleSignInProvider>(context, listen: false);
+          provider.googleLogin();
+        },
+        style: ElevatedButton.styleFrom(
+            primary: Colors.white,
+            onPrimary: Colors.black,
+            minimumSize: const Size(double.infinity, 50)),
+        icon: const FaIcon(FontAwesomeIcons.google, color: Colors.blue),
+      ),
+      const SizedBox(
+        height: 40.0,
+      ),
+      RichText(
+        text: const TextSpan(text: 'Already have an account?', children: [
+          TextSpan(
+            text: "Log in",
+            style: TextStyle(
+              decoration: TextDecoration.underline,
+            ),
+          ),
+        ]),
+      ),
+      const Spacer(),
+    ],
+  );
+}
