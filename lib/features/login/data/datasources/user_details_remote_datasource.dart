05f1678ccef5e6aaea5864c7cb34076717370350
Lior <lipocodes@yahoo.com>
2022-04-17 10:23:08 +0300

    During Equipment Inventory

--- lib/features/login/data/datasources/user_details_remote_datasource.dart
+++ lib/features/login/data/datasources/user_details_remote_datasource.dart
@@ -1,7 +1,39 @@
 import 'package:catchfish/features/login/domain/entities/user_entity.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
+//save/retreive user details from DB
 class UserDetailsRemoteDataSource {
   saveUserToDB(UserEntity userEntity) async {
-    print("aaaaaaaaaaaaaaaaaaa");
+    try {
+      final firestoreInstance = FirebaseFirestore.instance;
+
+      QuerySnapshot res = await firestoreInstance
+          .collection("users")
+          .where('email', isEqualTo: userEntity.email)
+          .get();
+
+      //user already has a document on DB so we need to update the doc
+      if (res.docs.isNotEmpty) {
+        await FirebaseFirestore.instance
+            .collection('users')
+            .doc(res.docs[0].id)
+            .update({
+          'displayName': userEntity.displayName,
+          'email': userEntity.email,
+          'photoURL': userEntity.photoURL,
+          'phoneNumber': userEntity.phoneNumber
+        });
+      } else {
+        //need to add a new doc to DB
+        await FirebaseFirestore.instance.collection('users').add({
+          'displayName': userEntity.displayName,
+          'email': userEntity.email,
+          'photoURL': userEntity.photoURL,
+          'phoneNumber': userEntity.phoneNumber
+        });
+      }
+    } catch (e) {
+      print("eeeeeeeeeeeeeeeeeeeee=" + e.toString());
+    }
   }
 }
