1ff20fde13021092e387dc81e66327ece3aea020
Lior <lipocodes@yahoo.com>
2022-04-18 06:26:52 +0300

    During Equipment Inventory

--- lib/features/settings/data/datasources/remote_datasource.dart
+++ lib/features/settings/data/datasources/remote_datasource.dart
@@ -1,9 +1,24 @@
 import 'package:catchfish/features/settings/data/models/inventory_model.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class RemoteDataSources {
-  Future<InventoryModel> getInventoryDB(String email) async {
-    InventoryModel inventoryModel =
-        await InventoryModel(listInventory: ["ccccccccccccccccccc"]);
-    return inventoryModel;
+  /*Future<InventoryModel?>*/ getInventoryDB(String email) async {
+    try {
+      var r = await FirebaseFirestore.instance
+          .collection("users")
+          .where('email', isEqualTo: email)
+          .get();
+      List temp = r.docs[0]['inventory'];
+      List<String> listInventory = [];
+      for (int a = 0; a < temp.length; a++) {
+        listInventory.add(temp[a].toString());
+      }
+
+      InventoryModel inventoryModel =
+          InventoryModel(listInventory: listInventory);
+      return inventoryModel;
+    } catch (e) {
+      print("eeeeeeeeeeeeeeeeeee=" + e.toString());
+    }
   }
 }
--- lib/features/settings/presentation/pages/equipment_inventory.dart
+++ lib/features/settings/presentation/pages/equipment_inventory.dart
@@ -8,6 +8,7 @@ import 'package:easy_localization/easy_localization.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class EquipmentInventory extends StatefulWidget {
   const EquipmentInventory({Key? key}) : super(key: key);
@@ -19,15 +20,10 @@ class EquipmentInventory extends StatefulWidget {
 class _EquipmentInventoryState extends State<EquipmentInventory> {
   late PlaySound playSound;
   bool _showedWarningYet = false;
-  List images = [
-    "assets/images/lobby/scroll.jpg",
-    "assets/images/lobby/scroll.jpg"
-  ];
-  List<String> items = [
-    'Baits',
-    'abcdefghijklmnopqrstuvwx',
-  ];
-  List<int> quatities = [5, 3];
+  List<String> images = [];
+  List<String> items = [];
+  List<int> quatities = [];
+  late SharedPreferences prefs;
 
   getGridViewItems(BuildContext context, String gridItem) {
     showDialog(
@@ -81,11 +77,15 @@ class _EquipmentInventoryState extends State<EquipmentInventory> {
     Navigator.pushNamed(context, '/');
   }
 
+  initilizePrefs() async {
+    prefs = await SharedPreferences.getInstance();
+  }
+
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
-
+    initilizePrefs();
     BlocProvider.of<InventoryBloc>(context).add(EnteringInventoryEvent());
 
     playSound = PlaySound();
@@ -112,62 +112,85 @@ class _EquipmentInventoryState extends State<EquipmentInventory> {
     return BlocBuilder<InventoryBloc, InventoryState>(
       builder: (context, state) {
         if (state is EnteringInventoryState) {
-          print("ggggggggggggggg=" +
-              state.inventoryEntity.listInventory.toString());
-        }
-        return Container(
-          decoration: const BoxDecoration(
-            image: DecorationImage(
-              image: AssetImage(
-                //tenor.com
-                'assets/images/settings/bubbles.gif',
+          images = [];
+          items = [];
+          quatities = [];
+          List localListInventory = prefs.getStringList("inventory") ?? [];
+          //going over inventory items saved on DB
+          for (int a = 0; a < state.inventoryEntity.listInventory.length; a++) {
+            String temp = state.inventoryEntity.listInventory[a];
+            List<String> list1 = temp.split("^^^");
+            temp = localListInventory[a];
+            List<String> list2 = temp.split("^^^");
+            int lastUpdateInventoryDB = int.parse(list1[3]);
+            int lastUpdateInventoryPrefs = int.parse(list2[3]);
+            //only if item exits on both DB & Prefs & item on DB is newer, show it to the user
+            if (lastUpdateInventoryDB > lastUpdateInventoryPrefs) {
+              images.add(list1[0]);
+              items.add(list1[1]);
+              quatities.add(int.parse(list1[2]));
+            } else {
+              images.add(list2[0]);
+              items.add(list2[1]);
+              quatities.add(int.parse(list2[2]));
+            }
+          }
+          return Container(
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage(
+                  //tenor.com
+                  'assets/images/settings/bubbles.gif',
+                ),
+                fit: BoxFit.cover,
               ),
-              fit: BoxFit.cover,
             ),
-          ),
-          child: GridView.count(
-            crossAxisCount: 1,
-            childAspectRatio: (2),
-            children: List.generate(items.length, (index) {
-              return Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Container(
-                  decoration: BoxDecoration(
-                      color: Colors.transparent,
-                      // Red border with the width is equal to 5
-                      border: Border.all(width: 3, color: Colors.grey)),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      SizedBox(
-                          width: 64,
-                          height: 64,
-                          child: Image.asset(images[index])),
-                      Text(
-                          items[index].length > 24
-                              ? items[index].substring(0, 24)
-                              : items[index],
+            child: GridView.count(
+              crossAxisCount: 1,
+              childAspectRatio: (2),
+              children: List.generate(items.length, (index) {
+                return Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                    decoration: BoxDecoration(
+                        color: Colors.transparent,
+                        // Red border with the width is equal to 5
+                        border: Border.all(width: 3, color: Colors.grey)),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        SizedBox(
+                            width: 64,
+                            height: 64,
+                            child: Image.network(images[index])),
+                        Text(
+                            items[index].length > 24
+                                ? items[index].substring(0, 24)
+                                : items[index],
+                            style: const TextStyle(
+                              fontSize: 28.0,
+                              color: Colors.yellow,
+                              fontFamily: 'skullsandcrossbones',
+                            )),
+                        Text(
+                          "quantity".tr() + quatities[index].toString(),
                           style: const TextStyle(
                             fontSize: 28.0,
-                            color: Colors.yellow,
+                            color: Colors.white,
                             fontFamily: 'skullsandcrossbones',
-                          )),
-                      Text(
-                        "quantity".tr() + quatities[index].toString(),
-                        style: const TextStyle(
-                          fontSize: 28.0,
-                          color: Colors.white,
-                          fontFamily: 'skullsandcrossbones',
+                          ),
                         ),
-                      ),
-                    ],
+                      ],
+                    ),
                   ),
-                ),
-              );
-            }).toList(),
-          ),
-        );
+                );
+              }).toList(),
+            ),
+          );
+        } else {
+          return Container();
+        }
       },
     );
   }
