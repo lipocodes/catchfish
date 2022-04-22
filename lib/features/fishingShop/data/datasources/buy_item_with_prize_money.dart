c507660f42e37cd4b671cb94db12140fd28dd203
Lior <lipocodes@yahoo.com>
2022-04-22 08:03:43 +0300

    Building the Fishing Shop

--- lib/features/fishingShop/data/datasources/buy_item_with_prize_money.dart
+++ lib/features/fishingShop/data/datasources/buy_item_with_prize_money.dart
@@ -4,18 +4,55 @@ import 'package:shared_preferences/shared_preferences.dart';
 class BuyItemWithMoneyPrizeRemoteDatasource {
   Future<RetreivePrizeModel> buyItem(
       String id, String image, String title, double price) async {
-    //First we update the shared prefs
+    BuyItemWithMoneyPrizeLocalDatasource buyItemWithMoneyPrizeLocalDatasource =
+        BuyItemWithMoneyPrizeLocalDatasource();
+    int inventoryMoney =
+        await buyItemWithMoneyPrizeLocalDatasource.retreiveMoneyPref();
+    int inventoryBaits =
+        await buyItemWithMoneyPrizeLocalDatasource.retreiveBaitsPref();
+    int inventoryXP =
+        await buyItemWithMoneyPrizeLocalDatasource.retreiveXPPref();
+    int inventoryLastUpdate =
+        await buyItemWithMoneyPrizeLocalDatasource.retreiveLastUpdatePref();
+    print("aaaaaaaaaaaaaaaaaaaaaaa=" + inventoryMoney.toString());
+    print("bbbbbbbbbbbbbbbbbbbb=" + inventoryBaits.toString());
+    print("cccccccccccccccccc=" + inventoryXP.toString());
+    print("ddddddddddddddddddd=" + inventoryLastUpdate.toString());
 
     RetreivePrizeModel retreivePrizeEntity = RetreivePrizeModel(
-        inventoryMoney: 150,
-        inventoryBaits: 140,
-        inventoryXP: 130,
+        inventoryMoney: 200,
+        inventoryBaits: 190,
+        inventoryXP: 180,
         lastPrizeValuesUpdateDB: DateTime.now().millisecondsSinceEpoch);
     return retreivePrizeEntity;
   }
 }
 
 class BuyItemWithMoneyPrizeLocalDatasource {
+  Future<int> retreiveMoneyPref() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    int inventoryMoney = prefs.getInt("inventoryMoney") ?? 0;
+    return inventoryMoney;
+  }
+
+  Future<int> retreiveBaitsPref() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    int inventoryBaits = prefs.getInt("inventoryBaits") ?? 0;
+    return inventoryBaits;
+  }
+
+  Future<int> retreiveXPPref() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    int inventoryXP = prefs.getInt("inventoryXP") ?? 0;
+    return inventoryXP;
+  }
+
+  Future<int> retreiveLastUpdatePref() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    int inventoryLastUpdate = prefs.getInt("lastPrizeValuesUpdateDB") ?? 0;
+    return inventoryLastUpdate;
+  }
+
   updatePrefs(int inventoryMoney, int inventoryBaits, int inventoryXP) async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
     await prefs.setInt("inventoryMoney", inventoryMoney);
