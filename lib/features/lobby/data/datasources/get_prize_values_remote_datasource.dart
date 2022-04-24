eec7e4b677efaa9ea6856106185e894207603909
Lior <lipocodes@yahoo.com>
2022-04-24 11:46:41 +0300

    Building the Tokens module

--- lib/features/lobby/data/datasources/get_prize_values_remote_datasource.dart
+++ lib/features/lobby/data/datasources/get_prize_values_remote_datasource.dart
@@ -4,18 +4,27 @@ import 'dart:convert';
 
 class GetPrizeValuesRemoteDatasource {
   Future<PrizeValuesEntity> getPrizeValuesDB(String email) async {
-    var t = await FirebaseFirestore.instance
-        .collection("users")
-        .where('email', isEqualTo: email)
-        .get();
+    try {
+      var t = await FirebaseFirestore.instance
+          .collection("users")
+          .where('email', isEqualTo: email)
+          .get();
 
-    PrizeValuesEntity prizeValuesEntity = PrizeValuesEntity(
-        inventoryMoney: t.docs[0].data()['prizeValues']['inventoryMoney'],
-        inventoryBaits: t.docs[0].data()['prizeValues']['inventoryBaits'],
-        inventoryXP: t.docs[0].data()['prizeValues']['inventoryXP'],
-        lastPrizeValuesUpdateDB: t.docs[0].data()['prizeValues']
-            ['lastPrizeValuesUpdateDB']);
-    return prizeValuesEntity;
+      PrizeValuesEntity prizeValuesEntity = PrizeValuesEntity(
+          inventoryMoney: t.docs[0].data()['prizeValues']['inventoryMoney'],
+          inventoryBaits: t.docs[0].data()['prizeValues']['inventoryBaits'],
+          inventoryXP: t.docs[0].data()['prizeValues']['inventoryXP'],
+          lastPrizeValuesUpdateDB: t.docs[0].data()['prizeValues']
+              ['lastPrizeValuesUpdateDB']);
+      return prizeValuesEntity;
+    } catch (e) {
+      PrizeValuesEntity prizeValuesEntity = PrizeValuesEntity(
+          inventoryMoney: 0,
+          inventoryBaits: 0,
+          inventoryXP: 0,
+          lastPrizeValuesUpdateDB: 0);
+      return prizeValuesEntity;
+    }
   }
 
   savePrizeValuesDB(String email, PrizeValuesEntity prizeValuesEntity) async {
