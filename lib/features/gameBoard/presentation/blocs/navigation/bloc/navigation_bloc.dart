f8e55d79b3cd52b8383c6c2a568f3557d4c0c57a
Lior <lipocodes@yahoo.com>
2022-05-17 15:45:43 +0300

    During Navigation

--- lib/features/gameBoard/presentation/blocs/navigation/bloc/navigation_bloc.dart
+++ lib/features/gameBoard/presentation/blocs/navigation/bloc/navigation_bloc.dart
@@ -11,8 +11,11 @@ class NavigationBloc extends Bloc<NavigationEvent, NavigationState> {
   final AudioCache audioCache = AudioCache(prefix: "assets/sounds/gameBoard/");
   AudioPlayer audioPlayer1 = AudioPlayer();
   AudioPlayer audioPlayer2 = AudioPlayer();
-  playBackgroundAudio(String engineSound) async {
-    audioPlayer1 = await audioCache.play("ignition.mp3");
+  playBackgroundAudio(bool needIgnition, String engineSound) async {
+    if (needIgnition) {
+      audioPlayer1 = await audioCache.play("ignition.mp3");
+    }
+
     audioPlayer2 = await audioCache.loop(engineSound);
   }
 
@@ -52,20 +55,28 @@ class NavigationBloc extends Bloc<NavigationEvent, NavigationState> {
         } else {
           isBoatRunning = true;
           statusGear = "N";
-          playBackgroundAudio("engine.mp3");
+          playBackgroundAudio(true, "N.mp3");
         }
         emit(IgnitionState(isBoatRunning: true, statusGear: statusGear));
       } else if (event is GearEvent) {
         if (event.selectedNewPosition == "F2" && statusGear == "F1") {
           statusGear = event.selectedNewPosition;
+          stopBackgroundAudio();
+          playBackgroundAudio(false, "F2.mp3");
         } else if (event.selectedNewPosition == "F1" &&
             (statusGear == "F2" || statusGear == "N")) {
           statusGear = event.selectedNewPosition;
+          stopBackgroundAudio();
+          playBackgroundAudio(false, "F1.mp3");
         } else if (event.selectedNewPosition == "N" &&
             (statusGear == "F1" || statusGear == "R")) {
           statusGear = event.selectedNewPosition;
+          stopBackgroundAudio();
+          playBackgroundAudio(false, "N.mp3");
         } else if (event.selectedNewPosition == "R" && statusGear == "N") {
           statusGear = event.selectedNewPosition;
+          stopBackgroundAudio();
+          playBackgroundAudio(false, "R.mp3");
         }
         emit(GearState(isBoatRunning: isBoatRunning, statusGear: statusGear));
       }
