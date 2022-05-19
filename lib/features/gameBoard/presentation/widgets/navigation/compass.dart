7e0b7ffbc5bf1956130bcff157164cbc3896b6f6
Lior <lipocodes@yahoo.com>
2022-05-19 11:29:09 +0300

    During Navigation

--- lib/features/gameBoard/presentation/widgets/navigation/compass.dart
+++ lib/features/gameBoard/presentation/widgets/navigation/compass.dart
@@ -1,15 +1,19 @@
 import 'package:flutter/material.dart';
 
 Widget compass(BuildContext context, double angle) {
-  return Transform.rotate(
-    angle: -angle,
-    child: Image.asset(
-      //pixabay.com
-      'assets/images/gameBoard/compass.png',
-      fit: BoxFit.fill,
-      height: double.infinity,
-      width: double.infinity,
-      alignment: Alignment.center,
-    ),
+  return Column(
+    children: [
+      Transform.rotate(
+        angle: -angle,
+        child: Image.asset(
+          //pixabay.com
+          'assets/images/gameBoard/compass.png',
+          fit: BoxFit.fill,
+          height: double.infinity,
+          width: double.infinity,
+          alignment: Alignment.center,
+        ),
+      ),
+    ],
   );
 }
