029dccea197436f6126e1680602bea3e50ade167
Lior <lipocodes@yahoo.com>
2022-06-02 14:45:40 +0300

    During Pulse Generator

--- lib/features/gameBoard/presentation/widgets/fishing/pulse_generator.dart
+++ lib/features/gameBoard/presentation/widgets/fishing/pulse_generator.dart
@@ -1,34 +1,64 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 
 Widget pulseGenerator(double angle) {
-  return Stack(
-    alignment: Alignment.center,
+  return Row(
+    mainAxisAlignment: MainAxisAlignment.spaceAround,
     children: [
-      SizedBox(
-        height: 150.0,
-        width: 200.0,
-        child: Image.asset(
-          //pixabay.com
-          'assets/images/gameBoard/gauge.png',
-          fit: BoxFit.fill,
-          height: double.infinity,
-          width: double.infinity,
-          alignment: Alignment.center,
-        ),
+      Stack(
+        alignment: Alignment.center,
+        children: [
+          SizedBox(
+            height: 150.0,
+            width: 200.0,
+            child: Image.asset(
+              //pixabay.com
+              'assets/images/gameBoard/gauge.png',
+              fit: BoxFit.fill,
+              height: double.infinity,
+              width: double.infinity,
+              alignment: Alignment.center,
+            ),
+          ),
+          SizedBox(
+            height: 70.0,
+            width: 20.0,
+            child: Transform.rotate(
+              angle: angle,
+              child: Image.asset(
+                //pixabay.com
+                'assets/images/gameBoard/hand.png',
+                fit: BoxFit.fill,
+                height: double.infinity,
+                width: double.infinity,
+                alignment: Alignment.center,
+              ),
+            ),
+          ),
+        ],
       ),
       SizedBox(
-        height: 70.0,
-        width: 20.0,
-        child: Transform.rotate(
-          angle: angle,
-          child: Image.asset(
-            //pixabay.com
-            'assets/images/gameBoard/hand.png',
-            fit: BoxFit.fill,
-            height: double.infinity,
-            width: double.infinity,
-            alignment: Alignment.center,
-          ),
+        height: 120.0,
+        width: 136.0,
+        child: Stack(
+          alignment: Alignment.center,
+          children: [
+            Image.asset(
+              //pixabay.com
+              'assets/images/gameBoard/redButton.png',
+              fit: BoxFit.fill,
+              height: double.infinity,
+              width: double.infinity,
+              alignment: Alignment.center,
+            ),
+            Text(
+              "catch".tr(),
+              style: const TextStyle(
+                  fontSize: 24.0,
+                  color: Colors.white,
+                  fontWeight: FontWeight.w700),
+            ),
+          ],
         ),
       ),
     ],
