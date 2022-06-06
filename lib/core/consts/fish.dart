1e0decf76ca8b21e968a469fcc87e10bfa1a596f
Lior <lipocodes@yahoo.com>
2022-06-06 12:18:01 +0300

    During Pulse Generator

---
+++ lib/core/consts/fish.dart
@@ -0,0 +1,29 @@
+//https://daviddagim.co.il/product-category/fresh-fish/%d7%93%d7%92%d7%99%d7%9d-%d7%a9%d7%9c%d7%9e%d7%99%d7%9d/page/2/
+List<String> fishCategoryA = [
+  "Barracuda^^^250^^^2500^^^barracuda",
+  "Halibut^^^270^^^1500^^^halibut",
+  "Cod^^^500^^^5000^^^cod"
+];
+List<String> fishCategoryB = [
+  "Norwegian Salmon^^^105^^^1500^^^norwegian_salmon"
+      "Idela^^^130^^^3500^^^idela",
+  "Solefish^^^196^^^600^^^solefish",
+];
+List<String> fishCategoryC = [
+  "Sargo^^^10^^^100^^^sargo",
+  "Sardin^^^15^^^150^^^sardin",
+  "Mullet^^^24^^^500^^^mullet",
+  "Tilapia^^^24^^^450^^^tilapia",
+  "Dennis^^^27^^^400^^^dennis",
+  "Levrek^^^35^^^250^^^levrek",
+  "Red Tilapia^^^40^^^700^^^red_tilapia",
+  "Pink Trout^^^46^^^450^^^pink_trout",
+  "Barramundi^^^46^^^550^^^barramundi",
+  "Basa Catfish^^^47^^^700^^^basa_catfish",
+  "Amberjack^^^48^^^400^^^amberjack",
+  "Mackerel^^^48^^^400^^^mackerel",
+  "Meagre^^^52^^^600^^^meagre",
+  "Grouper^^^75^^^500^^^grouper",
+  "Red Mullet^^^80^^^500^^^red_mullet",
+  "Carp^^^80^^^2000^^^carp"
+];
